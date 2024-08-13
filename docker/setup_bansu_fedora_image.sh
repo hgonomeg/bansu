c598e2efba69be7e5ff0ae3bbfd64097298626f0
Jakub Smulski <hgonomeg@gmail.com>
2024-08-13 23:50:19 +0200

    Fix Acedrg path resolution

--- docker/setup_bansu_fedora_image.sh
+++ docker/setup_bansu_fedora_image.sh
@@ -17,7 +17,7 @@ initial_setup() {
     echo "#!/usr/bin/sh" > /usr/bin/ccp4-python
     echo "export CCP4=/usr" >> /usr/bin/ccp4-python
     echo "export CBIN=/usr/bin/" >> /usr/bin/ccp4-python
-    echo "export CLIBD_MON=/usr/share/monomers" >> /usr/bin/ccp4-python
+    echo "export CLIBD_MON=/usr/share/monomers/" >> /usr/bin/ccp4-python
     echo "exec /usr/bin/python3 \"\$@\"" >> /usr/bin/ccp4-python
     chmod +x /usr/bin/ccp4-python
 
