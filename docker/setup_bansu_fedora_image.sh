267cd317b4327977a0eb91276b632032e910ae24
Jakub Smulski <hgonomeg@gmail.com>
2024-10-15 00:29:27 +0200

    Add a comment

--- docker/setup_bansu_fedora_image.sh
+++ docker/setup_bansu_fedora_image.sh
@@ -21,7 +21,7 @@ download_all() {
     # Acedrg
     # do_wget https://ccp4forge.rc-harwell.ac.uk/ccp4/acedrg/-/archive/main/acedrg-${ACEDRG_VER}.tar.gz &&\
     # tar -xf acedrg-${ACEDRG_VER}.tar.gz
-    echo Checking-out acedrg with breezy...
+    echo Checking-out acedrg with breezy (be patient, this may take a long time)...
     brz checkout https://fg.oisin.rc-harwell.ac.uk/anonscm/bzr/acedrg/trunk/ acedrg-${ACEDRG_VER} || exit 7
 
 
